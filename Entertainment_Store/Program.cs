de04bec98dfbd2ef897ea3e26598e49565a3af9d
James McNeil <mcneiljt@jacks.sfasu.edu>
2021-10-15 11:52:01 -0500

    Add project files.

---
+++ Entertainment_Store/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entertainment_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
